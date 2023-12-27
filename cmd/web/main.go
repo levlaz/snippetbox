4fc0259fb39241751813dee20143930d673ccad7
Lev Lazinskiy <lev@dagger.io>
2023-12-27 18:00:57 -0500

    serve static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,14 +6,21 @@ import (
 )
 
 func main() {
-	// use http.NewServeMux() func to init a new servemux, then
-	// register home function as handler for "/" URL pattern
 	mux := http.NewServeMux()
+
+	// create file server to serve files out of "./ui/static" dir
+	// note path is relative to project dir root
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// register file server as handler for all URL paths that
+	// start with "static"
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// register other application routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// print log message to say the server is up
 	log.Print("starting server on http://localhost:4000")
 
 	// use http.ListenAndServe() func to start web server.
