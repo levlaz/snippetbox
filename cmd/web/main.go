20e72181c78b5cbef79f696e9bf6ccc9289e5e40
Lev Lazinskiy <lev@dagger.io>
2024-01-17 16:11:24 -0800

    refactor to move routes to its own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,26 +27,11 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	// create file server to serve files out of "./ui/static" dir
-	// note path is relative to project dir root
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	// register file server as handler for all URL paths that
-	// start with "static"
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// register other application routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	logger.Info("starting server", slog.String("addr", ":4000"))
 
 	// use http.ListenAndServe() func to start web server.
 	// pass in address and servemux, log error and exit.
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,3 +1,17 @@
 package main
 
-// routes() returns servemux containing our application routes
+import "net/http"
+
+// return servemux containing our application routes
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
