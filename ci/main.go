bdaed2180a6b3fa854f8174b8c7cf5f12100ea12
Lev Lazinskiy <lev@dagger.io>
2024-03-18 11:01:02 +0100

    update dagger module

--- ci/main.go
+++ ci/main.go
@@ -6,34 +6,28 @@ import (
 	"time"
 )
 
-type Ci struct {
-	Ctr         *Container
-	Dir         *Directory
-	dockerToken Optional[*Secret]
+type Ci struct{}
+
+func (m *Ci) base() *Container {
+	return dag.Container().From("golang:alpine").
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("snippetbox-go-mod")).
+		WithMountedCache("/go/build-cache", dag.CacheVolume("snippetbox-go-build")).
+		WithEnvVariable("GOCACHE", "/go/build-cache").
+		WithExec([]string{"apk", "add", "tree"}).
+		WithExec([]string{"apk", "add", "mysql-client"})
 }
 
-func (m *Ci) initBaseImage() {
-	if m.Ctr == nil {
-		m.Ctr = dag.Container().From("golang:alpine").
-			WithMountedCache("/go/pkg/mod", dag.CacheVolume("snippetbox-go-mod")).
-			WithMountedCache("/go/build-cache", dag.CacheVolume("snippetbox-go-build")).
-			WithEnvVariable("GOCACHE", "/go/build-cache").
-			WithExec([]string{"apk", "add", "tree"}).
-			WithExec([]string{"apk", "add", "mysql-client"})
-	}
+// Lint
+func (m *Ci) Lint(ctx context.Context, dir *Directory) (string, error) {
+	return dag.Golang().
+		WithProject(dir).
+		GolangciLint(ctx)
 }
 
 // Run test suite
 func (m *Ci) Test(ctx context.Context, dir *Directory) (string, error) {
-	m.initBaseImage()
-
-	ci := &Ci{
-		Ctr: m.Ctr,
-		Dir: dir,
-	}
-
-	return ci.Ctr.
-		WithDirectory("/src", ci.Dir).
+	return m.base().
+		WithDirectory("/src", dir).
 		WithWorkdir("/src").
 		WithExec([]string{"go", "test", "./cmd/web"}).
 		Stdout(ctx)
@@ -44,29 +38,33 @@ func (m *Ci) Test(ctx context.Context, dir *Directory) (string, error) {
 func (m *Ci) Ci(
 	ctx context.Context,
 	dir *Directory,
-	token Optional[*Secret],
-	commit Optional[string],
+	// +optional
+	token *Secret,
+	// +optional
+	// +default="latest"
+	commit string,
 ) string {
-	m.initBaseImage()
-
-	ci := &Ci{
-		Ctr: m.Ctr,
-		Dir: dir,
-	}
 
 	var output string
 
-	// run tests
-	testOutput, err := m.Test(ctx, ci.Dir)
+	// run linter
+	lintOutput, err := m.Lint(ctx, dir)
 	if err != nil {
-		return fmt.Sprint(err)
+		fmt.Sprint(err)
+	}
+	output = output + "\n" + lintOutput
+
+	// run tests
+	testOutput, err := m.Test(ctx, dir)
+	if err != nil {
+		fmt.Sprint(err)
 	}
 	output = output + "\n" + testOutput
 
 	// publish container
 	publishOutput, err := m.Publish(ctx, dir, token, commit)
 	if err != nil {
-		return fmt.Sprint(err)
+		fmt.Sprint(err)
 	}
 	output = output + "\n" + publishOutput
 
@@ -77,54 +75,35 @@ func (m *Ci) Ci(
 func (m *Ci) Publish(
 	ctx context.Context,
 	dir *Directory,
-	token Optional[*Secret],
-	commit Optional[string],
+	// +optional
+	token *Secret,
+	// +optional
+	// +default="latest"
+	commit string,
 ) (string, error) {
-	m.initBaseImage()
 
-	ci := &Ci{
-		Ctr: m.Ctr,
-		Dir: dir,
-	}
+	if token != nil {
+		ctr := m.base().
+			WithDirectory("/src", dir).
+			WithRegistryAuth("docker.io", "levlaz", token)
 
-	dockerToken, isset := token.Get()
-	gitCommit := commit.GetOr("latest")
-
-	if isset {
-
-		tags := [2]string{"latest", gitCommit}
-
-		ctr := ci.Ctr.
-			WithDirectory("/src", ci.Dir).
-			WithRegistryAuth("docker.io", "levlaz", dockerToken)
-
-		for _, tag := range tags {
-			addr, err := ctr.Publish(ctx, fmt.Sprintf("levlaz/snippetbox:%s", tag))
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("Published: ", addr)
+		addr, err := ctr.Publish(ctx, fmt.Sprintf("levlaz/snippetbox:%s", commit))
+		if err != nil {
+			return "", fmt.Errorf("%s", err)
 		}
 
-		return fmt.Sprintf("Published: %s", tags), nil
+		return fmt.Sprintf("Published: %s", addr), nil
 	}
 
 	return "Must pass registry token to publish", nil
 }
 
 // Serve development site
-// example usage: "dagger up -m ci --port 4000:4000 serve --dir=."
+// example usage: "dagger call serve --dir=. up."
 func (m *Ci) Serve(dir *Directory) *Service {
-	m.initBaseImage()
-
-	ci := &Ci{
-		Ctr: m.Ctr,
-		Dir: dir,
-	}
-
-	return ci.Ctr.
+	return m.base().
 		WithServiceBinding("db", dag.Mariadb().Serve()).
-		WithDirectory("/src", ci.Dir).
+		WithDirectory("/src", dir).
 		WithWorkdir("/src").
 		WithExposedPort(4000).
 		WithEnvVariable("CACHEBUSTER", time.Now().String()).
@@ -132,4 +111,12 @@ func (m *Ci) Serve(dir *Directory) *Service {
 		WithExec([]string{"sh", "-c", "mysql -h db -u root snippetbox < internal/db/seed.sql"}).
 		WithExec([]string{"go", "run", "./cmd/web", "--dsn", "web:pass@tcp(db)/snippetbox?parseTime=true"}).
 		AsService()
+}
+
+// Debug Build Container
+func (m *Ci) Debug(dir *Directory) *Container {
+	return m.base().
+		WithServiceBinding("db", dag.Mariadb().Serve()).
+		WithDirectory("/src", dir).
+		WithWorkdir("/src")
 }
