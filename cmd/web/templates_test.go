53454120da3f28ff3c7c48da814110a32dd7593e
Lev Lazinskiy <lev@dagger.io>
2024-10-15 14:20:56 -0700

    add first unit test

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	// init new time.Time object and pass into humanDate()
+	tm := time.Date(2024, time.May, 15, 10, 0, 0, 0, time.UTC)
+	hd := humanDate(tm)
+
+	// check that output string has expected format
+	expected := "15 May 2024 at 10:00"
+	if hd != expected {
+		t.Errorf("got %q; want %q", hd, expected)
+	}
+}
