c8dd8ccd206d3a381522bf1212b321e4d9970efc
Lev Lazinskiy <lev@levlaz.org>
2025-08-26 14:29:30 -0700

    update mariadb version
    use the new version that supports debug capabilities

--- .dagger/main.go
+++ .dagger/main.go
@@ -190,7 +190,7 @@ func (m *Snippetbox) Debug(
 	database *dagger.Service,
 ) *dagger.Container {
 	if database == nil {
-		database = dag.Mariadb().Debug()
+		database = dag.Mariadb().Debug().AsService()
 	}
 	return m.base().
 		WithServiceBinding("db", database).
